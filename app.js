9b97f11b72b588aa48359ddd1ae5c78df299571b
meta-ela <anxhelameta@live.it>
2022-05-17 16:32:14 +0200

    Adds variables

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 // ` 
-const nomeUtente = prompt("What's your name?");
+const nomeUtente = prompt("Tell me about you. What's your name?");
+const cognomeUtente = prompt("Tell me about you. What's your surname?");
+const etaUtente = prompt("Tell me about you. How old are you?");
 
 
 
 // elemento id in html
-const spanDatiUtente = document.getElementById("dati-utente");
+const divDatiUtente = document.getElementById("dati-utente");
+const divEtaUtente = document.getElementById("eta-utente");
 
 
 //valore proprietà .innerHTML 
-spanDatiUtente.innerHTML = `${nomeUtente}`
+divDatiUtente.innerHTML = `${nomeUtente} ${cognomeUtente}`;
+divEtaUtente.innerHTML = `You are ${etaUtente} years old`
