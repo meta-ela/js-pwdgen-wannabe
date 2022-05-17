9277535c47d0f6da432f1b37049c3650bd8a2291
meta-ela <anxhelameta@live.it>
2022-05-17 17:47:35 +0200

    variables correction

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 // ` 
-const nomeUtente = prompt("Tell me about you. What's your name?");
-const cognomeUtente = prompt("Tell me about you. What's your surname?");
-const annoNascitaUtente = prompt("Tell me about you. What is your year of birth?");
-const etaUtente = 2022 - annoNascitaUtente;
-const coloreUtente = prompt("Tell me about you. What's your favorite color?");
+const nomeUtente = prompt("What's your name?");
+const cognomeUtente = prompt("What's your surname?")
+const coloreUtente = prompt("What's your favorite color?");
 
 
 
 // elemento id in html
 const divDatiUtente = document.getElementById("dati-utente");
-const divEtaUtente = document.getElementById("eta-utente");
-const divColoreUtente = document.getElementById("colore-utente");
 
 
 //valore proprietà .innerHTML 
-divDatiUtente.innerHTML = `${nomeUtente} ${cognomeUtente}`;
-divEtaUtente.innerHTML = `You are ${etaUtente} years old`;
-divColoreUtente.innerHTML = `Your favorite color is ${coloreUtente}`;
+divDatiUtente.innerHTML = `${nomeUtente}${cognomeUtente}${coloreUtente}22`;
+
+console.log(`${nomeUtente} ${cognomeUtente} ${coloreUtente}22`);
