8c9950e3d4aa2b82d3d3df2348741e8ef26b5dbd
meta-ela <anxhelameta@live.it>
2022-05-17 16:45:06 +0200

    Adds age and color variables

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 // ` 
 const nomeUtente = prompt("Tell me about you. What's your name?");
 const cognomeUtente = prompt("Tell me about you. What's your surname?");
-const etaUtente = prompt("Tell me about you. How old are you?");
+const annoNascitaUtente = prompt("Tell me about you. What is your year of birth?");
+const etaUtente = 2022 - annoNascitaUtente;
+const coloreUtente = prompt("Tell me about you. What's your favorite color?");
 
 
 
 // elemento id in html
 const divDatiUtente = document.getElementById("dati-utente");
 const divEtaUtente = document.getElementById("eta-utente");
+const divColoreUtente = document.getElementById("colore-utente");
 
 
 //valore proprietà .innerHTML 
 divDatiUtente.innerHTML = `${nomeUtente} ${cognomeUtente}`;
-divEtaUtente.innerHTML = `You are ${etaUtente} years old`
+divEtaUtente.innerHTML = `You are ${etaUtente} years old`;
+divColoreUtente.innerHTML = `Your favorite color is ${coloreUtente}`;
